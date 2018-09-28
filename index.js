388f4d5da98f01b3d12d8b70b4eb013a531f5966
Christian Broberg <cb@webhouse.dk>
2018-09-28 22:23:31 +0200

    Added log very simple functionality

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env /usr/local/bin/node
 
+const log = require('./utils/log').log
+var moment = require('moment')
+moment.locale('da')
+
 var exec = require('child_process').exec
 
 const _keepalive = 5
@@ -12,10 +16,12 @@ var _clientId = 'senti-' + Math.random().toString(16).substr(2, 8)
 
 function updateClient() {
 	console.log('Updating client ...')
+	log()
 	client.publish('sensor/test', 'Client restarted')
 	exec('bash updateclient.sh', function (error, stdout, stderr) {		
 		if (error) {
 			console.log(error.code)
+			log()
 		}
 	})
 }
@@ -33,12 +39,12 @@ var client = mqtt.connect('mqtt://hive.senti.cloud', {
 })
 
 client.publish('sensor/status', 'online', { retain: true })
-client.publish('sensor/status/' + _clientId, 'online', { retain: true })
+client.publish('sensor/status' + _clientId, 'online', { retain: true })
 
 client.on('connect', function () {
 	client.subscribe('sensor/test', function (err) {
 		if (!err) {
-			client.publish('sensor/test', 'Hello from Senti-in-a-Box client ID: ' + _clientId)
+			client.publish('sensor/test', 'Hello from Senti-in-a-Box ID: ' + _clientId)
 			client.publish('sensor/test', 'Keep alive = ' + _keepalive)
 			client.subscribe('sensor/update')
 			counter++
@@ -47,7 +53,8 @@ client.on('connect', function () {
 })
 
 client.on('message', function (topic, message) {
-	console.log(message.toString())	
+	console.log(message.toString())
+	log()
 	if (message.toString() === 'now') {		
 		updateClient()
 	}
@@ -55,22 +62,27 @@ client.on('message', function (topic, message) {
 
 client.on('offline', function () {	
 	console.log('We are offline')
+	log()
 })
 
 client.on("error", function (error) {
-	console.log("ERROR: ", error);
+	console.log("ERROR: ", error)
+	log()
 })
 
 client.on('reconnect', function () {
 	console.log("Reconnected")
+	log()
 })
 
 client.on('disconnect', function () {
 	console.log("Disconnected ...")
+	log()
 })
 
 client.on('close', function () {	
 	console.log('We are closing')
+	log()
 })
 
 client.on('packetreceive', function (packet) {
@@ -80,7 +92,10 @@ client.on('packetreceive', function (packet) {
 client.on('packetsend', function (packet) {
 	packets++
 	if (counter > 0) {
-        console.log('Client:', _clientId, 'Connection:', counter, ' Ping:', packets)        
+		console.log('ID:', _clientId, '- Con:', counter, '- Ping:', packets, '-', moment().format('L - HH:mm:ss'))
+		// console.log(moment().format('LLLL'))
+		// console.log(moment().format('L - HH:mm:ss'))
+		// log('ID:', _clientId, '- Con:', counter, '- Ping:', packets, '-', moment().format('L - HH:mm:ss'))
+		log()
 	}
-	// console.log('Packet sent! ', packet)
 })
---
+++ utils/log.js
@@ -0,0 +1,26 @@
+var moment = require('moment')
+moment.locale('da')
+
+moment().format('L')
+
+var fileName = __dirname + '/../logs/' + moment().format('L') + '.log' 
+
+var fs = require('fs')
+var util = require('util')
+// var log_file = fs.createWriteStream(__dirname + '/../logs/debug.log', { flags: 'a' })
+var log_file = fs.createWriteStream(fileName, { flags: 'a' })
+ // Or 'w' to truncate the file every time the process starts.
+var log_stdout = process.stdout
+
+function log(arguments) {
+	console.log = function () {
+		log_file.write(util.format.apply(null, arguments) + '\n')
+		log_stdout.write(util.format.apply(null, arguments) + '\n')
+	}
+	console.error = console.log
+}
+
+
+module.exports = {
+	log: log
+}
