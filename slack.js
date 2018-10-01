ac96e2e8def9f921c486dd2dc2e624e8efbbba6c
Christian Broberg <cb@cb-pro.local>
2018-10-01 20:02:04 +0200

    Hole through :)

--- slack.js
+++ slack.js
@@ -1,18 +1,18 @@
 // curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' 
 
-// https://hooks.slack.com/services/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
+const channel = 'https://hooks.slack.com/services/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'
 
-const channel = 'https://hooks.slack.com/services/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'
 
 function postMessageToSlack(url, message) {
 	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 	var xmlhttp = new XMLHttpRequest()
 	xmlhttp.open('POST', url, false)
-	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    xmlhttp.setRequestHeader('Content-Type', 'application/json')
+	// xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 	xmlhttp.send(message)
 }
 
 // console.log(channels['cb'].value, channels['cb'].message)
 
-postMessageToSlack(channel, 'Testing')
+postMessageToSlack(channel, '{"text":"Hello, World!"}')
 
