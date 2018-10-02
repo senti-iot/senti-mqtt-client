f07d25af575150bcf13b9cba465702ebc0f40b4a
Christian Broberg <cb@webhouse.dk>
2018-10-02 02:44:54 +0200

    Focused client topic

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ client.on('message', function (topic, message) {
 	log()
 	if (topic.toString() === 'sensor/update') {
 		if (message.toString() === 'now') {		
-			client.publish('sensor/status', 'offline', { retain: false })
+			client.publish('sensor/status/' + clientId, 'offline', { retain: false })
 			updateClient()
 			postToSlack(channel, `{"text":"${clientId}: updating - ${dateTimeLog()}"}`)
 		}
