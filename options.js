a72512cd9dfa8141aca9f98b7720db1b3beda75f
Christian Broberg <cb@webhouse.dk>
2018-10-06 22:58:01 +0200

    ping 200

--- options.js
+++ options.js
@@ -38,7 +38,7 @@ const options = {
 	},
 	slackChannel: 'https://hooks.slack.com/services/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',
 	logLocale: 'da',
-	ping: 1000
+	ping: 200
 }
 
 module.exports = options
