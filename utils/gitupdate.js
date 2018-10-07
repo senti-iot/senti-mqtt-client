99515aee3eee684bb4f6a32559def400818b6e47
Christian Broberg <cb@webhouse.dk>
2018-10-07 23:03:23 +0200

    Prevent crash

--- utils/gitupdate.js
+++ utils/gitupdate.js
@@ -5,7 +5,7 @@ const log = require('./log')
 const gitUpdate = () => {
 	// Pulls all updates from the default tracked repo
 	git().pull()
-	console.log('git pull', stdout)
+	console.log('git pull')
 	log()
 }
 
