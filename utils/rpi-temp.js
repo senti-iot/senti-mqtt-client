2ffc7c32d2bdc430cf131ab4af06f424a9a144e3
Christian Broberg <cb@webhouse.dk>
2018-10-09 21:19:28 +0200

    Temp

--- utils/rpi-temp.js
+++ utils/rpi-temp.js
@@ -7,7 +7,7 @@ const rpiTemp = () => {
 		temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp'])
 		temp.stdout.on('data', function (data) {
 			tempOut = (data / 1000)
-			console.log(tempOut)
+			// console.log(tempOut)
 			return tempOut
 		})
 	} else return null
