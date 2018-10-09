75bcf9635a5b63c303f71c356208db8c8573d1a6
Christian Broberg <cb@webhouse.dk>
2018-10-09 21:18:16 +0200

    Now work

--- utils/rpi-temp.js
+++ utils/rpi-temp.js
@@ -4,9 +4,10 @@ const isPi = require('./rpi-detect')
 const rpiTemp = () => {
 	if (isPi()) {
 		let tempOut
-		let temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp'])
+		temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp'])
 		temp.stdout.on('data', function (data) {
 			tempOut = (data / 1000)
+			console.log(tempOut)
 			return tempOut
 		})
 	} else return null
