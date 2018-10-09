68de66e5608c6f0cd58f622eb470bdfc021a4c18
Christian Broberg <cb@webhouse.dk>
2018-10-09 21:39:42 +0200

    With async/await

--- utils/rpi-temp.js
+++ utils/rpi-temp.js
@@ -7,10 +7,31 @@ const rpiTemp = () => {
 		temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp'])
 		temp.stdout.on('data', function (data) {
 			tempOut = (data / 1000)
-			// console.log(tempOut)
 			return tempOut
 		})
 	} else return null
 }
 
+
 module.exports = rpiTemp
+
+function resolveAfter2Seconds() {
+	return new Promise(resolve => {
+		if (isPi()) {
+			let tempOut
+			temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp'])
+			temp.stdout.on('data', function (data) {
+				tempOut = (data / 1000)
+				resolve(tempOut)
+			})
+		}
+	})
+}
+
+async function asyncCall() {
+	console.log('calling');
+	var result = await resolveAfter2Seconds()
+	console.log(result)	
+}
+
+asyncCall()
