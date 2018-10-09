3548e6130996162093c9bf0f7161c3e25af53112
Christian Broberg <cb@webhouse.dk>
2018-10-09 22:52:48 +0200

    .toString()

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ console.log('STARTING SENTI MQTT CLIENT SERVICES ...')
 const rpiTemperature = async(log, topic) => {
 	let result = await rpiTemp()
 	if (topic) {
-		client.publish('senti/sensor/sentiwi/8020/temperature', result, { retain: false })
+		client.publish('senti/sensor/sentiwi/8020/temperature', result.toString(), { retain: false })
 	}
 	if (log) console.log(result)
 }
